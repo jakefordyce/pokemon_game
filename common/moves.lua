c129f18cddc42130d4d91db90987db69ac57e63b
Jacob Fordyce <jfordyce2@GAIG.COM>
2021-08-19 09:23:43 -0400

    changed how some data was declared and renamed state lua

--- common/moves.lua
+++ common/moves.lua
@@ -2,22 +2,22 @@ local M = {}
 
 --List of all of the available moves
 
-effects = {}
-effects[0] = {}
-effects[0]["id"] = 1
-effects[0]["target"] = 1
-effects[0]["param1"] = 100
-effects[0]["param2"] = 1
+meffects = {}
+meffects[0] = {}
+meffects[0].id = 1
+meffects[0].target = 1
+meffects[0].param1 = 100
+meffects[0].param2 = 1
 
 M[1] = {
-	["name"] = "tackle",
-	["effects"] = effects,
-	["cooldown"] = 0,
-	["default"] = true,
-	["type"] = 1,
+	name = "tackle",
+	effects = meffects,
+	cooldown = 0,
+	default = true,
+	type = 1,
 	description = function(moveLevel)
 		damage = 100 + (moveLevel * 5)
-		return "Deals "..damage.."% of Attack to target enemy."
+		return "Attacks 1 enemy for "..damage.."% of Attack."
 	end
 }
 
