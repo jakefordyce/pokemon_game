7fb72606199c76bd05e57c9ae7b1007248fb8a8e
Jacob Fordyce <jfordyce2@GAIG.COM>
2021-08-19 12:11:34 -0400

    use state to load pokemon in battle

--- common/game_state.lua
+++ common/game_state.lua
@@ -1,5 +1,12 @@
 local M = {}
 
+--battle info
+M.player_mon1 = 0 --this is the index of M.pokemon
+M.player_mon2 = nil
+M.player_mon3 = nil
+M.player_mon4 = nil
+
+M.battle_num = 0
 
 
 --List of all of the player's pokemon.
@@ -17,7 +24,23 @@ M.pokemon[0] = {
 	move1 = 0, --this is the index from the pokemon's learned_moves.
 	move2 = 0,
 	move3 = 0,
-	move4 = 0
+	move4 = 0,
+	rune1 = 0, --this is the index from M.runes.
+	rune2 = nil,
+	rune3 = nil,
+	rune4 = nil,
+	rune5 = nil,
+	rune6 = nil,
+	hp = 100,
+	attack = 5,
+	defense = 5,
+	spattack = 5,
+	spdefense = 5,
+	speed = 100,
+	acc = 0,
+	res = 0,
+	crit_chance = 5,
+	crit_damage = 50
 }
 
 --List of all of the player's runes.
