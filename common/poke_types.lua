d48892e106f24547649147ed5f313e8e29d66aee
Jake Fordyce <JakeFordyce78@gmail.com>
2021-09-14 15:09:26 -0400

    display current move at top of move list when clicked

---
+++ common/poke_types.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+M.names = {}
+
+M.names[1] = "Normal"
+M.names[2] = "Fighting"
+M.names[3] = "Flying"
+M.names[4] = "Poison"
+M.names[5] = "Earth"
+M.names[6] = "Bug"
+M.names[7] = "Steel"
+M.names[8] = "Dragon"
+M.names[9] = "Fire"
+M.names[10] = "Water"
+M.names[11] = "Grass"
+M.names[12] = "Electric"
+M.names[13] = "Psychic"
+M.names[14] = "Ice"
+M.names[15] = "Dark"
+
+
+return M
