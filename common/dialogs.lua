9cf19101c5f380f0cc214a968246a5caf4afd552
Jake Fordyce <JakeFordyce78@gmail.com>
2021-10-15 10:46:27 -0400

    add pokemon font

--- common/dialogs.lua
+++ common/dialogs.lua
@@ -1,13 +1,13 @@
 local M = {}
 
 M[0] = {
-	text = "Hello. Thank you for coming all this way. We need all of the help we can get. Did anyone tell you why I asked you to come here?",
+	text = "OAK: Hello. Thank you for coming all this way. We need all of the help we can get. Did anyone tell you why I asked you to come here?",
 	next = nil,
 	has_choices = true
 }
 
 M[1] = {
-	text = "Right so let's get to it. You'll need some Pokemon before you go any further. Here, you can have your pick of these. Go ahead and take a full party, you'll need it.",
+	text = "OAK: Right so let's get to it. You'll need some Pokemon before you go any further. Here, you can have your pick of these. Go ahead and take a full party, you'll need it.",
 	next = nil,
 	has_choices = false
 }
