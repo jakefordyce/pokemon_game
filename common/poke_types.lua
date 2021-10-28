08c2f9f4f0ee71884a40984a91a0987c9eff8a32
jakefordyce <jakefordyce78@gmail.com>
2021-10-28 16:15:24 -0400

    added type strengths and weaknesses

--- common/poke_types.lua
+++ common/poke_types.lua
@@ -1,22 +1,81 @@
 local M = {}
 
-M.names = {}
 
-M.names[1] = "normal"
-M.names[2] = "fighting"
-M.names[3] = "flying"
-M.names[4] = "poison"
-M.names[5] = "earth"
-M.names[6] = "bug"
-M.names[7] = "steel"
-M.names[8] = "dragon"
-M.names[9] = "fire"
-M.names[10] = "water"
-M.names[11] = "grass"
-M.names[12] = "electric"
-M.names[13] = "psychic"
-M.names[14] = "ice"
-M.names[15] = "dark"
+M[1] = {
+	name = "normal",
+	strengths = {},
+	weaknesses = {}
+}
+M[2] = {
+	name ="fighting",
+	strengths = {7, 15},
+	weaknesses = {3, 13}
+}
+M[3] = {
+	name = "flying",
+	strengths = {2, 6},
+	weaknesses = {12, 14}
+}
+M[4] = {
+	name = "poison",
+	strengths = {6, 11},
+	weaknesses = {5, 13}
+}
+M[5] = {
+	name = "earth",
+	strengths = {4, 12},
+	weaknesses = {10, 11}
+}
+M[6] = {
+	name = "bug",
+	strengths = {13, 15},
+	weaknesses = {3, 4}
+}
+M[7] = {
+	name = "steel",
+	strengths = {12, 14},
+	weaknesses = {2, 8}
+}
+M[8] = {
+	name = "dragon",
+	strengths = {7, 9},
+	weaknesses = {14, 15}
+}
+M[9] = {
+	name = "fire",
+	strengths = {11, 14},
+	weaknesses = {8, 10}
+}
+M[10] = {
+	name = "water",
+	strengths = {5, 9},
+	weaknesses = {11, 12}
+}
+M[11] = {
+	name = "grass",
+	strengths = {5, 10},
+	weaknesses = {4, 9}
+}
+M[12] = {
+	name = "electric",
+	strengths = {3, 10},
+	weaknesses = {5, 7}
+}
+M[13] = {
+	name = "psychic",
+	strengths = {2, 4},
+	weaknesses = {6, 15}
+}
+M[14] = {
+	name = "ice",
+	strengths = {3, 8},
+	weaknesses = {7, 9}
+}
+M[15] = {
+	name = "dark",
+	strengths = {8, 13},
+	weaknesses = {2, 6}
+}
 
 
 return M
