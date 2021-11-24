e039ec289f046500bcdced52f1859907c6fa8334
jakefordyce <jakefordyce78@gmail.com>
2021-11-24 13:52:51 -0500

    tweak status effect durations

--- battle/status_effect.go
+++ battle/status_effect.go
@@ -37,8 +37,8 @@ embedded_components {
   id: "label"
   type: "label"
   data: "size {\n"
-  "  x: 128.0\n"
-  "  y: 32.0\n"
+  "  x: 50.0\n"
+  "  y: 20.0\n"
   "  z: 0.0\n"
   "  w: 0.0\n"
   "}\n"
@@ -68,17 +68,17 @@ embedded_components {
   "}\n"
   "leading: 1.0\n"
   "tracking: 0.0\n"
-  "pivot: PIVOT_CENTER\n"
+  "pivot: PIVOT_E\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   "line_break: false\n"
-  "text: \"0\"\n"
+  "text: \"1\"\n"
   "font: \"/menu/menu_details_outlined.font\"\n"
-  "material: \"/builtins/fonts/font.material\"\n"
+  "material: \"/builtins/fonts/label.material\"\n"
   ""
   position {
-    x: 13.0
-    y: -12.0
-    z: 1.0
+    x: 16.0
+    y: -11.0
+    z: 0.2
   }
   rotation {
     x: 0.0
