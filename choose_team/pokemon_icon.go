391fc7e80c0979c34ea6781f68d7dd05f34b1219
jakefordyce <jakefordyce78@gmail.com>
2021-11-24 13:26:12 -0500

    show pokemon level on icons

--- choose_team/pokemon_icon.go
+++ choose_team/pokemon_icon.go
@@ -79,3 +79,77 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "background"
+  type: "sprite"
+  data: "tile_set: \"/menu/menu.atlas\"\n"
+  "default_animation: \"poke_background_3\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: -0.2
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "mon_level"
+  type: "label"
+  data: "size {\n"
+  "  x: 40.0\n"
+  "  y: 20.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "scale {\n"
+  "  x: 0.8\n"
+  "  y: 0.8\n"
+  "  z: 1.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "color {\n"
+  "  x: 1.0\n"
+  "  y: 1.0\n"
+  "  z: 1.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "outline {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "shadow {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "leading: 1.0\n"
+  "tracking: 0.0\n"
+  "pivot: PIVOT_E\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "line_break: false\n"
+  "text: \"5\"\n"
+  "font: \"/menu/menu_details_outlined.font\"\n"
+  "material: \"/builtins/fonts/label.material\"\n"
+  ""
+  position {
+    x: 29.0
+    y: -24.0
+    z: 0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
